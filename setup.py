e0e45f01a3a6d7c127ea528deaf76ca70ebe666b
Bruno Di Giorgi <bruno@brunodigiorgi.it>
2017-04-05 20:53:05 +0200

    new structure

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
+from setuptools import find_packages
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
 setup(
     name='pyPitchTracking',
-    ext_modules = cythonize([
-        Extension("pyPitchTracking", 
-                  sources=["pyPitchTracking.pyx", 
-                           "PitchTracking/src/PitchTracker.cpp",
-                           "PitchTracking/src/utils.cpp",
-                           ], 
-                  include_dirs=['PitchTracking/include', numpy.get_include()],
+    version='0.1',
+    description='Python Pitch Tracking',
+    author='Bruno Di Giorgi',
+    author_email='bruno@brunodigiorgi.it',
+    url='https://github.com/brunodigiorgi/pyPitchTracking',
+    license="GPLv2",
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[],
+    extras_require={},
+    ext_modules=cythonize([
+        Extension("pyPitchTracking.PyPitchTrackerYin",
+                  sources=["pyPitchTracking/PyPitchTrackerYin/PyPitchTrackerYin.pyx",
+                           "pyPitchTracking/PyPitchTrackerYin/PitchTracking/src/PitchTracker.cpp",
+                           "pyPitchTracking/PyPitchTrackerYin/PitchTracking/src/utils.cpp",
+                           ],
+                  include_dirs=['pyPitchTracking/PyPitchTrackerYin/PitchTracking/include', numpy.get_include()],
                   extra_compile_args=['-std=gnu++11', '-stdlib=libc++', '-mmacosx-version-min=10.7'],
                   extra_link_args=['-mmacosx-version-min=10.7'],
                   language='c++',
                   )
-        ])
+    ])
 )
